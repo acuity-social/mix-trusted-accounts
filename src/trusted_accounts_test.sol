2691328264855f465f838d00fac9c439c2bc4f9a
Jonathan Brown <jbrown@mix-blockchain.org>
2018-04-30 15:15:23 +0700

    Add testing stub.

---
+++ src/trusted_accounts_test.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.23;
+
+import "ds-test/test.sol";
+
+
+/**
+ * @title TrustedAccountsTest
+ * @author Jonathan Brown <jbrown@mix-blockchain.org>
+ * @dev Testing contract for TrustedAccounts.
+ */
+contract TrustedAccountsTest is DSTest {
+}
